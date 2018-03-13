5f7ac5efb4e36166e31622f14b7255a1caa85f81
pmkary <pmk@nobody.invalid>
2018-03-13 13:20:48 +0330

    new github remover

--- extension/sites/github.com.js
+++ extension/sites/github.com.js
@@ -12,6 +12,7 @@
         'p.f6.text-gray.mt-2.mb-0 > span.d-inline-block.mr-3',
         '.stars',
         'div.d-flex.flex-justify-between.flex-items-start.mb-1 > a',
+        '#js-pjax-container > div > div.columns > div.column.three-fourths.codesearch-results > div > ul > div > div.col-2.text-right.pt-1.pr-3.pt-2'
     ]
 
 //
